9bd68381cdefa0cb32c070dcd37cef3ffc9de39e
Mahdi Farnaghi <mahdi.farnaghi@yahoo.com>
2021-10-08 15:16:56 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MahdiFarnaghi/geoso',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
