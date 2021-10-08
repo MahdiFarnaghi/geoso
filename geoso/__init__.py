211fd643e0dc05925f4983407ab5a21c88e0c95e
Mahdi Farnaghi <mahdi.farnaghi@yahoo.com>
2021-10-08 15:29:20 +0200

    Bump version: 0.0.2 → 0.0.3

--- geoso/__init__.py
+++ geoso/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Mahdi Farnaghi"""
 __email__ = 'mahdi.farnaghi@outlook.com'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from .twitter.reader_writer import (
     twitter_export_db_to_csv,
